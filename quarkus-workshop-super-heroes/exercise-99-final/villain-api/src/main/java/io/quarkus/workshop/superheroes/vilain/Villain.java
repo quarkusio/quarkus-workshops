bd43554535ab28db89b5654855e8715f9845afde
Clement Escoffier <clement.escoffier@gmail.com>
2019-09-11 15:41:32 +0200

    Imports the super villains!

---
+++ quarkus-workshop-super-heroes/exercise-99-final/villain-api/src/main/java/io/quarkus/workshop/superheroes/vilain/Villain.java
@@ -0,0 +1,19 @@
+package io.quarkus.workshop.superheroes.vilain;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Entity
+public class Villain extends PanacheEntity {
+
+    public String name;
+    public String otherName;
+    public int level;
+    public String picture;
+
+    @Column(columnDefinition="TEXT")
+    public String powers;
+
+}
