98027a8065c475d52c30884b771334501b4cc5a8
Antonio Goncalves <antonio.goncalves@gmail.com>
2019-09-26 17:35:44 +0200

    Fixing compilation

--- quarkus-workshop-super-heroes/exercise-99-final/fight-api/src/test/java/io/quarkus/workshop/superheroes/fight/client/MockHeroService.java
+++ quarkus-workshop-super-heroes/exercise-99-final/fight-api/src/test/java/io/quarkus/workshop/superheroes/fight/client/MockHeroService.java
@@ -9,7 +9,6 @@ import javax.enterprise.context.ApplicationScoped;
 public class MockHeroService implements HeroService {
 
     public static final String DEFAULT_HERO_NAME = "Super Baguette";
-    public static final String DEFAULT_HERO_OTHER_NAME = "Super Baguette Tradition";
     public static final String DEFAULT_HERO_PICTURE = "super_baguette.png";
     public static final String DEFAULT_HERO_POWERS = "eats baguette really quickly";
     public static final int DEFAULT_HERO_LEVEL = 42;
@@ -18,7 +17,6 @@ public class MockHeroService implements HeroService {
     public Hero findRandomHero() {
         Hero hero = new Hero();
         hero.setName(DEFAULT_HERO_NAME);
-        hero.setOtherName(DEFAULT_HERO_OTHER_NAME);
         hero.setPicture(DEFAULT_HERO_PICTURE);
         hero.setPowers(DEFAULT_HERO_POWERS);
         hero.setLevel(DEFAULT_HERO_LEVEL);
--- quarkus-workshop-super-heroes/exercise-99-final/fight-api/src/test/java/io/quarkus/workshop/superheroes/fight/client/MockVillainService.java
+++ quarkus-workshop-super-heroes/exercise-99-final/fight-api/src/test/java/io/quarkus/workshop/superheroes/fight/client/MockVillainService.java
@@ -9,7 +9,6 @@ import javax.enterprise.context.ApplicationScoped;
 public class MockVillainService implements VillainService {
 
     public static final String DEFAULT_VILLAIN_NAME = "Super Chocolatine";
-    public static final String DEFAULT_VILLAIN_OTHER_NAME = "Super Chocolatine chocolate in";
     public static final String DEFAULT_VILLAIN_PICTURE = "super_chocolatine.png";
     public static final String DEFAULT_VILLAIN_POWERS = "does not eat pain au chocolat";
     public static final int DEFAULT_VILLAIN_LEVEL = 42;
@@ -18,7 +17,6 @@ public class MockVillainService implements VillainService {
     public Villain findRandomVillain() {
         Villain villain = new Villain();
         villain.setName(DEFAULT_VILLAIN_NAME);
-        villain.setOtherName(DEFAULT_VILLAIN_OTHER_NAME);
         villain.setPicture(DEFAULT_VILLAIN_PICTURE);
         villain.setPowers(DEFAULT_VILLAIN_POWERS);
         villain.setLevel(DEFAULT_VILLAIN_LEVEL);
