933c55d302da9001abcd4eb2142a8bfa20fced4f
Antonio Goncalves <antonio.goncalves@gmail.com>
2019-10-02 15:46:49 +0200

    Fixing REST client call

--- quarkus-workshop-super-heroes/api-fight/src/main/java/io/quarkus/workshop/superheroes/fight/client/HeroService.java
+++ quarkus-workshop-super-heroes/api-fight/src/main/java/io/quarkus/workshop/superheroes/fight/client/HeroService.java
@@ -7,7 +7,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/api/villains")
+@Path("/api/heroes")
 @Produces(MediaType.APPLICATION_JSON)
 @RegisterRestClient
 public interface HeroService {
