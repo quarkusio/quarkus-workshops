9403b7dd18fba3be69b13ebbda54151e015edcf2
Antonio Goncalves <antonio.goncalves@gmail.com>
2019-10-02 12:07:26 +0200

    Adding docs

--- quarkus-workshop-super-heroes/exercise-99-final/super-heroes-ui/src/app/fight/fight.component.ts
+++ quarkus-workshop-super-heroes/exercise-99-final/super-heroes-ui/src/app/fight/fight.component.ts
@@ -13,8 +13,8 @@ export class FightComponent implements OnInit {
   }
 
   ngOnInit() {
-    //this.fightService.apiFightsRandomfightersGet().subscribe(figthers => this.figthers = figthers);
-    this.figthers.hero = {name: 'Chewbacca', picture: 'https://www.superherodb.com/pictures2/portraits/10/050/10466.jpg', powers: 'Agility, Longevity, Marksmanship, Natural Weapons, Stealth, Super Strength, Weapons Master'};
-    this.figthers.villain = {name: 'Egg Fu', picture: 'https://www.superherodb.com/pictures2/portraits/11/050/11157.jpg', powers: 'Cold Resistance, Duplication, Explosion Manipulation, Jump, Natural Weapons, Power Suit, Resurrection, Super Strength, Technopath/Cyberpath, Toxin and Disease Resistance, Weapon-based Powers'};
+    this.fightService.apiFightsRandomfightersGet().subscribe(figthers => this.figthers = figthers);
+    // this.figthers.hero = {name: 'Chewbacca', picture: 'https://www.superherodb.com/pictures2/portraits/10/050/10466.jpg', powers: 'Agility, Longevity, Marksmanship, Natural Weapons, Stealth, Super Strength, Weapons Master'};
+    // this.figthers.villain = {name: 'Egg Fu', picture: 'https://www.superherodb.com/pictures2/portraits/11/050/11157.jpg', powers: 'Cold Resistance, Duplication, Explosion Manipulation, Jump, Natural Weapons, Power Suit, Resurrection, Super Strength, Technopath/Cyberpath, Toxin and Disease Resistance, Weapon-based Powers'};
   }
 }
