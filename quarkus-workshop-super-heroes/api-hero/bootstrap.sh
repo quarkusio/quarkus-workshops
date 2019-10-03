a8ad3c5501f41c69549618f060a5e4c10354fc50
Antonio Goncalves <antonio.goncalves@gmail.com>
2019-10-03 17:10:43 +0200

    Docs

--- quarkus-workshop-super-heroes/api-hero/bootstrap.sh
+++ quarkus-workshop-super-heroes/api-hero/bootstrap.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
+# tag::adocSnippet
 mvn io.quarkus:quarkus-maven-plugin:0.23.1:create \
     -DprojectGroupId=io.quarkus.workshop.super-heroes \
     -DprojectArtifactId=hero-api \
     -DclassName="io.quarkus.workshop.superheroes.hero.HeroResource" \
     -Dpath="/api/heroes"
+# end::adocSnippet
