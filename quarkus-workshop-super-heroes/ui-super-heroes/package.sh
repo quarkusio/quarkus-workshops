99e1d0287285ee922729aac41707bbe93ddcb59e
Clement Escoffier <clement.escoffier@gmail.com>
2019-10-06 19:56:02 +0200

    Fix base path in UI.

--- quarkus-workshop-super-heroes/ui-super-heroes/package.sh
+++ quarkus-workshop-super-heroes/ui-super-heroes/package.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 export DEST=../api-fight/src/main/resources/META-INF/resources/super-heroes
-ng build --prod --base-href /super-heroes
+ng build --prod --base-href "."
 rm -Rf ${DEST}
 cp -R dist/* ${DEST}
 
