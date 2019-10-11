1ad929d9256426267c1977032258f844bbf9a7cf
Antonio Goncalves <antonio.goncalves@gmail.com>
2019-10-11 16:12:53 +0200

    Docs

--- quarkus-workshop-super-heroes/super-heroes/rest-hero/src/main/resources/import.sql
+++ quarkus-workshop-super-heroes/super-heroes/rest-hero/src/main/resources/import.sql
@@ -5,12 +5,12 @@ INSERT INTO hero(id, name, otherName, picture, powers, level)
 VALUES (nextval('hibernate_sequence'), 'Angel Salvadore', 'Angel Salvadore Bohusk', 'https://www.superherodb.com/pictures2/portraits/10/050/1406.jpg', 'Animal Attributes, Animal Oriented Powers, Flight, Regeneration, Toxin and Disease Control', 4);
 INSERT INTO hero(id, name, otherName, picture, powers, level)
 VALUES (nextval('hibernate_sequence'), 'Bill Harken', '', 'https://www.superherodb.com/pictures2/portraits/10/050/1527.jpg', 'Super Speed, Super Strength, Toxin and Disease Resistance', 6);
+-- end::adocSQL[]
 INSERT INTO hero(id, name, otherName, picture, powers, level)
 VALUES (nextval('hibernate_sequence'), 'Warlock (Infinity Gauntlet)', 'Adam Warlock', 'https://www.superherodb.com/pictures2/portraits/11/050/13426.jpg',
         'Astral Projection, Biokinesis, Duplication, Element Control, Energy Beams, Energy Manipulation, Immortality, Matter Manipulation, Mind Control, Molecular Manipulation, Nigh-Omnipresent, Portal Creation, Reality Warping, Regeneration, Size Changing, Spatial Awareness, Super Speed, Super Strength, Telekinesis, Telepathy, Teleportation, Time Manipulation, Time Travel, Weather Control', 74);
 INSERT INTO hero(id, name, otherName, picture, powers, level)
 VALUES (nextval('hibernate_sequence'), 'Shazam (New 52)', 'Billy Batson', 'https://www.superherodb.com/pictures2/portraits/10/050/14714.jpg', 'Accelerated Healing, Durability, Element Control, Energy Absorption, Energy Blasts, Fire Resistance, Flight, Illumination, Invulnerability, Levitation, Magic, Marksmanship, Super Speed, Super Strength', 14);
--- end::adocSQL[]
 INSERT INTO hero(id, name, otherName, picture, powers, level)
 VALUES (nextval('hibernate_sequence'), 'Galadriel', '', 'https://www.superherodb.com/pictures2/portraits/11/050/11796.jpg', 'Danger Sense, Immortality, Intelligence, Invisibility, Magic, Precognition, Telekinesis, Telepathy', 17);
 INSERT INTO hero(id, name, otherName, picture, powers, level)
--- quarkus-workshop-super-heroes/super-heroes/rest-villain/src/main/resources/import.sql
+++ quarkus-workshop-super-heroes/super-heroes/rest-villain/src/main/resources/import.sql
@@ -1,9 +1,11 @@
+-- tag::adocSQL[]
 INSERT INTO villain(id, name, otherName, picture, powers, level)
 VALUES (nextval('hibernate_sequence'), 'Buuccolo', 'Majin Buu', 'https://www.superherodb.com/pictures2/portraits/11/050/15355.jpg', 'Accelerated Healing, Adaptation, Agility, Flight, Immortality, Intelligence, Invulnerability, Reflexes, Self-Sustenance, Size Changing, Spatial Awareness, Stamina, Stealth, Super Breath, Super Speed, Super Strength, Teleportation', 22);
 INSERT INTO villain(id, name, otherName, picture, powers, level)
 VALUES (nextval('hibernate_sequence'), 'Darth Vader', 'Anakin Skywalker', 'https://www.superherodb.com/pictures2/portraits/10/050/10444.jpg', 'Accelerated Healing, Agility, Astral Projection, Cloaking, Danger Sense, Durability, Electrokinesis, Energy Blasts, Enhanced Hearing, Enhanced Senses, Force Fields, Hypnokinesis, Illusions, Intelligence, Jump, Light Control, Marksmanship, Precognition, Psionic Powers, Reflexes, Stealth, Super Speed, Telekinesis, Telepathy, The Force, Weapons Master', 13);
 INSERT INTO villain(id, name, otherName, picture, powers, level)
 VALUES (nextval('hibernate_sequence'), 'The Rival (CW)', 'Edward Clariss', 'https://www.superherodb.com/pictures2/portraits/11/050/13846.jpg', 'Accelerated Healing, Agility, Bullet Time, Durability, Electrokinesis, Endurance, Enhanced Senses, Intangibility, Marksmanship, Phasing, Reflexes, Speed Force, Stamina, Super Speed, Super Strength', 10);
+-- end::adocSQL[]
 INSERT INTO villain(id, name, otherName, picture, powers, level)
 VALUES (nextval('hibernate_sequence'), 'William Stryker (FOX)', 'William Stryker, Jr.', 'https://www.superherodb.com/pictures2/portraits/10/050/13957.jpg', 'Intelligence, Intuitive aptitude, Marksmanship, Weapon-based Powers', 9);
 INSERT INTO villain(id, name, otherName, picture, powers, level)
