1f12770fde82788761c3c4240366b9a862dc4406
Clement Escoffier <clement.escoffier@gmail.com>
2019-10-13 18:56:29 +0200

    Stats microservice bootstrap

---
+++ quarkus-workshop-super-heroes/super-heroes/event-statistics/bootstrap.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# tag::adocSnippet[]
+mvn io.quarkus:quarkus-maven-plugin:0.24.0:create \
+    -DprojectGroupId=io.quarkus.workshop.super-heroes \
+    -DprojectArtifactId=event-statistics \
+    -DclassName="io.quarkus.workshop.superheroes.statistics.StatisticResource" \
+    -Dpath="/api/stats" \
+    -Dextensions="kafka, vertx, resteasy-jsonb, undertow-websockets"
+# end::adocSnippet[]
