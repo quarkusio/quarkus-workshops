befe8d3237e26e885096ee59f379ad39559d2bba
Emmanuel Bernard <emmanuel@hibernate.org>
2019-10-16 18:51:08 +0200

    Fix REST directory infos

--- quarkus-workshop-super-heroes/super-heroes/rest-hero/bootstrap.sh
+++ quarkus-workshop-super-heroes/super-heroes/rest-hero/bootstrap.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # tag::adocSnippet[]
+cd quarkus-workshop-super-heroes/super-heroes/rest-hero
 mvn io.quarkus:quarkus-maven-plugin:0.25.0:create \
     -DprojectGroupId=io.quarkus.workshop.super-heroes \
     -DprojectArtifactId=rest-hero \
