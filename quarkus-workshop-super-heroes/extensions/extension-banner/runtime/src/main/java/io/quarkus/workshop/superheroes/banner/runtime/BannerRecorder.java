a0ec1deea067d2088671ba300438ab4c488d291e
Clement Escoffier <clement.escoffier@gmail.com>
2019-10-17 11:03:09 +0200

    simpler extension

--- quarkus-workshop-super-heroes/extensions/extension-banner/runtime/src/main/java/io/quarkus/workshop/superheroes/banner/runtime/BannerRecorder.java
+++ quarkus-workshop-super-heroes/extensions/extension-banner/runtime/src/main/java/io/quarkus/workshop/superheroes/banner/runtime/BannerRecorder.java
@@ -1,12 +1,11 @@
 package io.quarkus.workshop.superheroes.banner.runtime;
 
-import io.quarkus.arc.runtime.BeanContainer;
 import io.quarkus.runtime.annotations.Recorder;
 
 @Recorder
 public class BannerRecorder {
 
-    public void configureBannerBean(BeanContainer container, String path) {
-        container.instance(BannerBean.class).setup(path);
+    public void print(String banner) {
+        System.err.println(banner);
     }
 }
