3bfbac3c35a27802b5a93534aa6bddb7048d9b72
Clement Escoffier <clement.escoffier@gmail.com>
2019-10-17 11:02:47 +0200

    rewrote banner extension

--- quarkus-workshop-super-heroes/extensions/extension-banner/deployment/src/main/java/io/quarkus/workshop/superheroes/banner/deployment/BannerProcessor.java
+++ quarkus-workshop-super-heroes/extensions/extension-banner/deployment/src/main/java/io/quarkus/workshop/superheroes/banner/deployment/BannerProcessor.java
@@ -1,33 +1,48 @@
 package io.quarkus.workshop.superheroes.banner.deployment;
 
-import io.quarkus.arc.deployment.AdditionalBeanBuildItem;
-import io.quarkus.arc.deployment.BeanContainerBuildItem;
-import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.annotations.ExecutionTime;
 import io.quarkus.deployment.annotations.Record;
-import io.quarkus.deployment.builditem.substrate.SubstrateResourceBuildItem;
-import io.quarkus.workshop.superheroes.banner.runtime.BannerBean;
+import io.quarkus.deployment.builditem.HotDeploymentWatchedFileBuildItem;
+import io.quarkus.deployment.util.FileUtil;
 import io.quarkus.workshop.superheroes.banner.runtime.BannerRecorder;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UncheckedIOException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BannerProcessor {
 
     @BuildStep
-    @Record(ExecutionTime.STATIC_INIT)
-    public void recordBannerBeanInitialization(BeanContainerBuildItem beanContainer, BannerRecorder recorder,
-        BannerConfig config) {
-        recorder.configureBannerBean(beanContainer.getValue(), config.file);
+    @Record(ExecutionTime.RUNTIME_INIT)
+    public void recordBanner(BannerRecorder recorder, BannerConfig config) {
+        String content = readBannerFile(config.path);
+        recorder.print(content);
     }
 
     @BuildStep
-    AdditionalBeanBuildItem registerBannerBean() {
-        return AdditionalBeanBuildItem.unremovableOf(BannerBean.class);
+    List<HotDeploymentWatchedFileBuildItem> watchBannerChanges(BannerConfig config) {
+        List<HotDeploymentWatchedFileBuildItem> watchedFiles = new ArrayList<>();
+        watchedFiles.add(new HotDeploymentWatchedFileBuildItem((config.path)));
+        return watchedFiles;
     }
 
-    @BuildStep
-    void addResourceToNativeImage(BuildProducer<SubstrateResourceBuildItem> resource, BannerConfig config) {
-        resource.produce(new SubstrateResourceBuildItem(config.file));
+    private String readBannerFile(String path) {
+        URL resource = Thread.currentThread().getContextClassLoader().getResource(path);
+        if (resource != null) {
+            try (InputStream is = resource.openStream()) {
+                byte[] content = FileUtil.readFileContents(is);
+                return new String(content, StandardCharsets.UTF_8);
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
+            }
+        } else {
+            throw new IllegalArgumentException("Cannot find the banner file: " + path);
+        }
     }
-
 
 }
