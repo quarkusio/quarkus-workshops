61e5e2d850a522e43476a21dfe0d5f4af21aed06
Emmanuel Bernard <emmanuel@hibernate.org>
2019-10-18 00:02:10 +0200

    Add extensions to the Villain project creation

--- quarkus-workshop-super-heroes/super-heroes/rest-villain/bootstrap.sh
+++ quarkus-workshop-super-heroes/super-heroes/rest-villain/bootstrap.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 # tag::adocSnippet[]
-mvn io.quarkus:quarkus-maven-plugin:0.25.0:create \
+./mvnw io.quarkus:quarkus-maven-plugin:0.25.0:create \
     -DprojectGroupId=io.quarkus.workshop.super-heroes \
     -DprojectArtifactId=rest-villain \
     -DclassName="io.quarkus.workshop.superheroes.villain.VillainResource" \
     -Dpath="/api/villains"
+./mvnw quarkus:add-extension -Dextensions="jdbc-postgresql,hibernate-orm-panache,hibernate-validator,quarkus-resteasy-jsonb,openapi"
 # end::adocSnippet[]
