359d6a69c51e91354dbaeffa31300930ec64c8c8
Clement Escoffier <clement.escoffier@gmail.com>
2019-10-18 16:42:10 +0200

    doc

--- quarkus-workshop-super-heroes/super-heroes/rest-hero/bootstrap.sh
+++ quarkus-workshop-super-heroes/super-heroes/rest-hero/bootstrap.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 # tag::adocSnippet[]
+# If not already created, create the structure:
+mkdir -p quarkus-workshop-super-heroes/super-heroes/rest-hero
+
 cd quarkus-workshop-super-heroes/super-heroes/rest-hero
 mvn io.quarkus:quarkus-maven-plugin:0.25.0:create \
     -DprojectGroupId=io.quarkus.workshop.super-heroes \
