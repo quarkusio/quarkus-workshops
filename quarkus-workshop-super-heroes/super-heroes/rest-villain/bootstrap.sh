2a1b4581b7b8d0ffdad25fc4795861b595b5bf9c
Clement Escoffier <clement.escoffier@gmail.com>
2019-10-18 20:50:14 +0200

    doc

--- quarkus-workshop-super-heroes/super-heroes/rest-villain/bootstrap.sh
+++ quarkus-workshop-super-heroes/super-heroes/rest-villain/bootstrap.sh
@@ -4,6 +4,6 @@ mvn io.quarkus:quarkus-maven-plugin:0.25.0:create \
     -DprojectGroupId=io.quarkus.workshop.super-heroes \
     -DprojectArtifactId=rest-villain \
     -DclassName="io.quarkus.workshop.superheroes.villain.VillainResource" \
-    -Dpath="/api/villains"
-./mvnw quarkus:add-extension -Dextensions="jdbc-postgresql,hibernate-orm-panache,hibernate-validator,quarkus-resteasy-jsonb,openapi"
+    -Dpath="/api/villains" \
+    -Dextensions="jdbc-postgresql,hibernate-orm-panache,hibernate-validator,quarkus-resteasy-jsonb,openapi"
 # end::adocSnippet[]
