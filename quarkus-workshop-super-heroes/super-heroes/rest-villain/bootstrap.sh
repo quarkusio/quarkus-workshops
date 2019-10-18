76d56c0b3acb577dde7b899ca61c06b41dc79f14
Emmanuel Bernard <emmanuel@hibernate.org>
2019-10-18 17:29:49 +0200

    Add extensions for Flight

--- quarkus-workshop-super-heroes/super-heroes/rest-villain/bootstrap.sh
+++ quarkus-workshop-super-heroes/super-heroes/rest-villain/bootstrap.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # tag::adocSnippet[]
-./mvnw io.quarkus:quarkus-maven-plugin:0.25.0:create \
+mvn io.quarkus:quarkus-maven-plugin:0.25.0:create \
     -DprojectGroupId=io.quarkus.workshop.super-heroes \
     -DprojectArtifactId=rest-villain \
     -DclassName="io.quarkus.workshop.superheroes.villain.VillainResource" \
