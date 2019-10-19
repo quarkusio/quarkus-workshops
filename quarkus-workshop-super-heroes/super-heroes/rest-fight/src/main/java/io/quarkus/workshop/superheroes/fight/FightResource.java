764d3a8bc20d3f0199ec41681e79aea6f46b55a0
Clement Escoffier <clement.escoffier@gmail.com>
2019-10-19 08:53:01 +0200

    doc

--- quarkus-workshop-super-heroes/super-heroes/rest-fight/src/main/java/io/quarkus/workshop/superheroes/fight/FightResource.java
+++ quarkus-workshop-super-heroes/super-heroes/rest-fight/src/main/java/io/quarkus/workshop/superheroes/fight/FightResource.java
@@ -1,12 +1,13 @@
 // tag::adocResource[]
 package io.quarkus.workshop.superheroes.fight;
 
-
+// end::adocResource[]
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.faulttolerance.Timeout;
 import org.eclipse.microprofile.metrics.MetricUnits;
 import org.eclipse.microprofile.metrics.annotation.Counted;
 import org.eclipse.microprofile.metrics.annotation.Timed;
+// tag::adocResource[]
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
 import org.eclipse.microprofile.openapi.annotations.media.Content;
