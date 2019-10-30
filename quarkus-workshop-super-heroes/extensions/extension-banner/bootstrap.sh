5ceaee8ec78baef5b0e8ca543bb9b1e739c336f4
Clement Escoffier <clement.escoffier@gmail.com>
2019-10-30 19:00:28 +0100

    update to Quarkus 0.27.0

--- quarkus-workshop-super-heroes/extensions/extension-banner/bootstrap.sh
+++ quarkus-workshop-super-heroes/extensions/extension-banner/bootstrap.sh
@@ -40,7 +40,7 @@ echo "<project xmlns='http://maven.apache.org/POM/4.0.0' xmlns:xsi='http://www.w
     </dependencyManagement>
 
     <properties>
-        <quarkus.version>0.25.0</quarkus.version>
+        <quarkus.version>0.27.0</quarkus.version>
         <surefire-plugin.version>2.22.0</surefire-plugin.version>
         <maven.compiler.source>1.8</maven.compiler.source>
         <maven.compiler.target>1.8</maven.compiler.target>
