6eb01dfbb2627526fbc59e867bc577920087b964
Clement Escoffier <clement.escoffier@gmail.com>
2020-01-24 17:06:58 +0100

    Update Quarkus version to 1.2.0.Final

--- quarkus-workshop-super-heroes/super-heroes/rest-hero/bootstrap.sh
+++ quarkus-workshop-super-heroes/super-heroes/rest-hero/bootstrap.sh
@@ -4,7 +4,7 @@
 mkdir -p quarkus-workshop-super-heroes/super-heroes/rest-hero
 
 cd quarkus-workshop-super-heroes/super-heroes/rest-hero
-mvn io.quarkus:quarkus-maven-plugin:1.0.0.CR1:create \
+mvn io.quarkus:quarkus-maven-plugin:1.2.0.Final:create \
     -DprojectGroupId=io.quarkus.workshop.super-heroes \
     -DprojectArtifactId=rest-hero \
     -DclassName="io.quarkus.workshop.superheroes.hero.HeroResource" \
