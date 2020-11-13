570fec87b5f41c9d9d35928eb1e1a8726df0caaf
Auri Munoz <amunozhe@redhat.com>
2020-11-13 18:09:04 +0100

    wip: fix github raw + fix publish.sh

--- quarkus-workshop-super-heroes/docs/publish.sh
+++ quarkus-workshop-super-heroes/docs/publish.sh
@@ -5,7 +5,7 @@
 ## WILL PUBLISH STUFF ON THE WEBSITE, DON'T SCREW UP
 export old=`pwd`
 #build doc
-#mvn clean generate-resources
+mvn clean generate-resources
 #publish doc
 cd /tmp
 rm -fR /tmp/bbvahackathon.github.io
@@ -13,10 +13,9 @@ git clone git@github.com:bbvahackathon/bbvahackathon.github.io.git
 cd $old
 #rm -fR /tmp/bbvahackathon.github.io
 rsync -avz ./target/generated-asciidoc/ /tmp/bbvahackathon.github.io
-#cp -r ./target/generated-asciidoc/ /tmp/bbvahackathon.github.io
-cp /tmp/bbvahackathon.github.io/generated-asciidoc/spine.html /tmp/bbvahackathon.github.io/index.html
+cp /tmp/bbvahackathon.github.io/spine.html /tmp/bbvahackathon.github.io/index.html
 cd /tmp/bbvahackathon.github.io
 git add .
-git commit -m "update Quarkus workshop"
-git push origin master
+git commit -m "regenerating docs for Quarkus workshop"
+git push origin -u master
 cd $old
