bfad356b441a589142eec641f47aa4b7585fb5e0
Auri Munoz <amunozhe@redhat.com>
2020-11-16 11:19:38 +0100

    refactor bootstrapping rest-hero doc

--- quarkus-workshop-super-heroes/super-heroes/rest-hero/bootstrap.sh
+++ quarkus-workshop-super-heroes/super-heroes/rest-hero/bootstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # tag::adocSnippet[]
 cd quarkus-workshop-super-heroes/super-heroes
-mvn io.quarkus:quarkus-maven-plugin:1.2.0.Final:create \
+mvn io.quarkus:quarkus-maven-plugin:1.9.2.Final:create \
     -DprojectGroupId=io.quarkus.workshop.super-heroes \
     -DprojectArtifactId=rest-hero \
     -DclassName="io.quarkus.workshop.superheroes.hero.HeroResource" \
