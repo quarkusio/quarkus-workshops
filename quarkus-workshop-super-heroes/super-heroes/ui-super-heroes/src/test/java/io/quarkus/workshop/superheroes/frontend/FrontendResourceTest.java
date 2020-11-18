78132f72a7878e15c0d9feffd7b6b42db4e000d3
cvicens <carlos.vicens.alonso@gmail.com>
2020-11-18 18:33:28 +0100

    fixed

--- quarkus-workshop-super-heroes/super-heroes/ui-super-heroes/src/test/java/io/quarkus/workshop/superheroes/frontend/FrontendResourceTest.java
+++ quarkus-workshop-super-heroes/super-heroes/ui-super-heroes/src/test/java/io/quarkus/workshop/superheroes/frontend/FrontendResourceTest.java
@@ -5,17 +5,18 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.instanceOf;
 
 @QuarkusTest
 public class FrontendResourceTest {
 
     @Test
-    public void testHelloEndpoint() {
+    public void testBasePathEndpoint() {
         given()
-          .when().get("/api/frontend")
+          .when().get("/api/frontend/basePath")
           .then()
              .statusCode(200)
-             .body(is("hello"));
+             .body(is(instanceOf(String.class)));
     }
 
 }
