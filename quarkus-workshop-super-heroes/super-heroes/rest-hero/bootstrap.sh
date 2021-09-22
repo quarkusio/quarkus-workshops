48c394314f20242847bd624b2d8be212982c16d4
Clement Escoffier <clement@apache.org>
2021-09-22 09:22:45 +0200

    Fix rest-villains path in doc (missing the last s)

--- quarkus-workshop-super-heroes/super-heroes/rest-hero/bootstrap.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# tag::adocSnippet[]
-cd quarkus-workshop-super-heroes/super-heroes
-mvn io.quarkus:quarkus-maven-plugin:2.2.3.Final:create \
-    -DprojectGroupId=io.quarkus.workshop.super-heroes \
-    -DprojectArtifactId=rest-hero \
-    -DclassName="io.quarkus.workshop.superheroes.hero.HeroResource" \
-    -Dpath="api/heroes"
-# end::adocSnippet[]
