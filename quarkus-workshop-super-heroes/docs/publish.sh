cc494b20bd3832b5d84e64dd9274638bc5bce7eb
Clement Escoffier <clement@apache.org>
2021-09-22 09:32:27 +0200

    Update publish.sh to publish the 2.0 version

--- quarkus-workshop-super-heroes/docs/publish.sh
+++ quarkus-workshop-super-heroes/docs/publish.sh
@@ -4,6 +4,7 @@
 # ONLY WORKS FROM THE DOCS DIR
 # WILL PUBLISH STUFF ON THE WEBSITE, DON'T SCREW UP
 export old=`pwd`
+export version=2.0
 #build doc
 mvn install
 #publish doc
@@ -13,11 +14,12 @@ git clone git@github.com:quarkusio/quarkus-workshops.git
 cd quarkus-workshops
 git checkout gh-pages
 cd $old
-rm -fR /tmp/quarkus-workshops/super-heroes
-rsync -avz ./target/generated-asciidoc/ /tmp/quarkus-workshops/super-heroes
-cp /tmp/quarkus-workshops/super-heroes/spine.html /tmp/quarkus-workshops/super-heroes/index.html 
+mkdir -p /tmp/quarkus-workshops/super-heroes/${version}
+rsync -avz ./target/generated-asciidoc/ /tmp/quarkus-workshops/super-heroes/${version}
+cp /tmp/quarkus-workshops/super-heroes/${version}/spine.html /tmp/quarkus-workshops/super-heroes/${version}/index.html
 cd /tmp/quarkus-workshops
 git add .
-git commit -m "update Quarkus workshop"
+git commit -m "update Quarkus workshop ${version}"
 git push origin gh-pages
 cd $old
+rm -fR /tmp/quarkus-workshops
