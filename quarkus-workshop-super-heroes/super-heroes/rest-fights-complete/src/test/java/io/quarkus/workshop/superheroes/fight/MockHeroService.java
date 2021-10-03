db3802ecfdcd1f8d6a813ee53471463583c73e0e
Clement Escoffier <clement@apache.org>
2021-10-03 10:22:42 +0200

    Quarkus 2.x rewrite

---
+++ quarkus-workshop-super-heroes/super-heroes/rest-fights-complete/src/test/java/io/quarkus/workshop/superheroes/fight/MockHeroService.java
@@ -0,0 +1,29 @@
+package io.quarkus.workshop.superheroes.fight;
+
+import io.quarkus.test.Mock;
+import io.quarkus.workshop.superheroes.fight.client.Hero;
+import io.quarkus.workshop.superheroes.fight.client.HeroService;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@Mock
+@ApplicationScoped
+@RestClient
+public class MockHeroService implements HeroService {
+
+    public static final String DEFAULT_HERO_NAME = "Super Baguette";
+    public static final String DEFAULT_HERO_PICTURE = "super_baguette.png";
+    public static final String DEFAULT_HERO_POWERS = "eats baguette really quickly";
+    public static final int DEFAULT_HERO_LEVEL = 42;
+
+    @Override
+    public Hero findRandomHero() {
+        Hero hero = new Hero();
+        hero.name = DEFAULT_HERO_NAME;
+        hero.picture = DEFAULT_HERO_PICTURE;
+        hero.powers = DEFAULT_HERO_POWERS;
+        hero.level = DEFAULT_HERO_LEVEL;
+        return hero;
+    }
+}
--- quarkus-workshop-super-heroes/super-heroes/ui-super-heroes/src/app/shared/api.module.ts
+++ quarkus-workshop-super-heroes/super-heroes/ui-super-heroes/src/app/shared/api.module.ts
@@ -13,7 +13,7 @@ import { FightService } from './api/fight.service';
     FightService ]
 })
 export class ApiModule {
-    public static forRoot(configurationFactory: () => Configuration): ModuleWithProviders {
+    public static forRoot(configurationFactory: () => Configuration): ModuleWithProviders<ApiModule> {
         return {
             ngModule: ApiModule,
             providers: [ { provide: Configuration, useFactory: configurationFactory } ]
