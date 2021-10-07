c3726a351f7aac3c8de2f224b895b6ed9739a652
Antonio Goncalves <antonio.goncalves@gmail.com>
2021-10-07 14:59:23 +0200

    Fix #114 Adding logs

--- quarkus-workshop-super-heroes/super-heroes/event-statistics/src/main/java/io/quarkus/workshop/superheroes/statistics/SuperStats.java
+++ quarkus-workshop-super-heroes/super-heroes/event-statistics/src/main/java/io/quarkus/workshop/superheroes/statistics/SuperStats.java
@@ -3,11 +3,16 @@ package io.quarkus.workshop.superheroes.statistics;
 import io.smallrye.mutiny.Multi;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.eclipse.microprofile.reactive.messaging.Outgoing;
+import org.jboss.logging.Logger;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 
 @ApplicationScoped
 public class SuperStats {
+
+    @Inject
+    Logger logger;
 
     private final Ranking topWinners = new Ranking(10);
     private final TeamStats stats = new TeamStats();
@@ -16,7 +21,8 @@ public class SuperStats {
     @Outgoing("team-stats")
     public Multi<Double> computeTeamStats(Multi<Fight> results) {
         return results
-            .onItem().transform(stats::add);
+            .onItem().transform(stats::add)
+            .invoke(() -> logger.info("Fight received. Computed the team statistics"));
     }
 
     @Incoming("fights")
@@ -27,7 +33,8 @@ public class SuperStats {
             .onItem().transformToMultiAndMerge(group ->
                 group
                     .onItem().scan(Score::new, this::incrementScore))
-            .onItem().transform(topWinners::onNewScore);
+            .onItem().transform(topWinners::onNewScore)
+            .invoke(() -> logger.info("Fight received. Computed the top winners"));
     }
 
     private Score incrementScore(Score score, Fight fight) {
