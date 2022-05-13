80262da0f6b01125477cac101ae3249496a64883
Antonio Goncalves <antonio.goncalves@gmail.com>
2022-05-13 16:57:10 +0200

    Upgrade Statistics

---
+++ quarkus-workshop-super-heroes/super-heroes/rest-fights/src/test/java/io/quarkus/workshop/superheroes/fight/FightResourceIT.java
@@ -0,0 +1,9 @@
+package io.quarkus.workshop.superheroes.fight;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class FightResourceIT extends FightResourceTest {
+
+    // Execute the same tests but in native mode.
+}
