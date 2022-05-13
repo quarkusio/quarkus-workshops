05a75df89122426740d7298a68c68a896dc90ad2
Antonio Goncalves <antonio.goncalves@gmail.com>
2022-05-13 05:52:03 +0200

    Updating Heroes

---
+++ quarkus-workshop-super-heroes/super-heroes/rest-heroes/src/test/java/io/quarkus/workshop/superheroes/hero/HeroResourceIT.java
@@ -0,0 +1,9 @@
+package io.quarkus.workshop.superheroes.hero;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class HeroResourceIT extends HeroResourceTest {
+
+    // Execute the same tests but in native mode.
+}
