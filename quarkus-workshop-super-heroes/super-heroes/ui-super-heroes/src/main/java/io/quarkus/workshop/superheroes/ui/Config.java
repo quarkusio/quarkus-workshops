8d6b945a6e7048c347215002860f64321c12a00b
Holly Cummins <hcummins@redhat.com>
2023-05-26 09:20:03 +0200

    Ensure UI works in native mode

--- quarkus-workshop-super-heroes/super-heroes/ui-super-heroes/src/main/java/io/quarkus/workshop/superheroes/ui/Config.java
+++ quarkus-workshop-super-heroes/super-heroes/ui-super-heroes/src/main/java/io/quarkus/workshop/superheroes/ui/Config.java
@@ -1,4 +1,12 @@
 package io.quarkus.workshop.superheroes.ui;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
+/*
+Why do we need to register this for reflection? Normally this would be automatic if
+we return it from a REST endpoint, but because we're handling our own
+object mapping, we need to do our own registering.
+ */
+@RegisterForReflection
 public record Config(String API_BASE_URL, boolean CALCULATE_API_BASE_URL) {
 }
