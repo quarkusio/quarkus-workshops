9e57cb18d52e2892c105bdab5e95040adab0e4df
Georgios Andrianakis <geoand@gmail.com>
2023-09-25 14:31:07 +0300

    Remove ITs that don't work because of injection in tests

--- quarkus-workshop-super-heroes/super-heroes/rest-fights/src/test/java/io/quarkus/workshop/superheroes/fight/FightResourceIT.java
+++
@@ -1,8 +0,0 @@
-package io.quarkus.workshop.superheroes.fight;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class FightResourceIT extends FightResourceTest {
-    // Execute the same tests but in packaged mode.
-}
--- quarkus-workshop-super-heroes/super-heroes/rest-narration/src/test/java/io/quarkus/workshop/superheroes/narration/NarrationResourceIT.java
+++
@@ -1,8 +0,0 @@
-package io.quarkus.workshop.superheroes.narration;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class NarrationResourceIT extends NarrationResourceTest {
-    // Execute the same tests but in packaged mode.
-}
