62ab0bb9b899e97ea02a33346eeecd8b125118f5
Holly Cummins <holly.k.cummins@googlemail.com>
2026-05-15 16:11:03 +0100

    Fix compile error

--- quarkus-workshop-super-heroes/docs/src/test/java/io/quarkus/workshop/docs/DocumentationTest.java
+++ quarkus-workshop-super-heroes/docs/src/test/java/io/quarkus/workshop/docs/DocumentationTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.*;
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.HashSet;
 import java.util.Set;
 
